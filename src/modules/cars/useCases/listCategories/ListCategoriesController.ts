049db327b72330c3c4e5fc73c527a40fc9bd5eb5
Bruno Silva <brs.rsilva@gmail.com>
2024-09-14 11:49:45 -0300

    refactor: :construction: listCategories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,4 +1,4 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
